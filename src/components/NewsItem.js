78c4a61cb96e7a5fed51571b69baebe32a310f8c
Mohd Kaish <kaishredmi743@gmail.com>
2026-08-12 01:39:05 +0530

    added infinite-scroll and loading-bar

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,9 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card">
-          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-success" style={{left:'90%',zIndex:'1'}}>{source}</span>
+          <div style={{display:'flex',justifyContent:'flex-end',position:'absolute',right:5 }}>
+          <span className="badge rounded-pill bg-success">{source}</span>
+          </div>
           <img
             src={imageUrl?imageUrl:"https://images.unsplash.com/photo-1504711434969-e33886168f5c"}
             alt="news"
